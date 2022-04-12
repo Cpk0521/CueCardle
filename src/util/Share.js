5ac7051844c6dfb1d497dce11b940df03fc73ff0
CPK <samkit157@gmail.com>
2022-04-12 15:51:34 +0800

    modal ui fix

--- src/util/Share.js
+++ src/util/Share.js
@@ -11,11 +11,11 @@ export const generateEmojiTile = (guesses) => {
 }
 
 export const CopyToClipBoard = (guesses, isWon, isLost) => {
-    navigator.clipboard.writeText(`CUE!Cardle #${Listindex + 1} ${isWon?guesses.length:isLost?'X':''}/6\n${generateEmojiTile(guesses)}\nhttps://cpk0521.github.io/CueCardle`);
+    navigator.clipboard.writeText(`CUE!Cardle #${Listindex + 1} ${isWon?guesses.length:isLost?'X':''}/6\n${generateEmojiTile(guesses)}\nhttps://cpk0521.github.io/CueCardle/`);
 }
 
 export const TweetShare = (guesses, isWon, isLost) => {
-    let ShareText = `CUE!Cardle %23${Listindex + 1}%20${isWon?guesses.length:isLost?'X':''}/6%0a${generateEmojiTile(guesses)}%0a&hashtags=キュー&url=https://cpk0521.github.io/CueCardle%0a`    
+    let ShareText = `CUE!Cardle %23${Listindex + 1}%20${isWon?guesses.length:isLost?'X':''}/6%0a${generateEmojiTile(guesses)}%0a&hashtags=キュー&url=https://cpk0521.github.io/CueCardle/%0a`    
     window.open(`https://twitter.com/intent/tweet?text=${ShareText}`, "_blank")
 }
 
