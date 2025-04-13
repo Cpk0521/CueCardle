6c61888f5dc373bad53e9568ea6293df87540c06
CPK <samkit157@gmail.com>
2025-04-13 20:34:06 +0800

    rename to 4

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ function App() {
               </svg>
             </a>
           </div>
-          <h1>CUE!Cardle³</h1>
+          <h1>CUE!Cardle⁴</h1>
           <div className="nav-right">
           <button onClick={switchTechModal}>
             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
--- src/util/Hello.js
+++ src/util/Hello.js
@@ -3,7 +3,7 @@ const clog = console.log;
 export default function Hello() {
     
     let log = [
-        `\n\n %c  %c   CUE!Cardle v3.0   %c  %c  https://github.com/Cpk0521  %c \n\n`,
+        `\n\n %c  %c   CUE!Cardle v4.0   %c  %c  https://github.com/Cpk0521  %c \n\n`,
         'background: #00ffff; padding:5px 0;',
         'color: #00ffff; background: #030307; padding:5px 0;',
         'background: #00ffff; padding:5px 0;',
